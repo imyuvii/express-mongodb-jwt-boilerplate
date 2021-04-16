6168674b526e48838147dc63d23f55b0d971bc69
Yuvraj Jhala <png625@gmail.com>
2021-04-16 13:07:27 +0530

    Feature: ignore files

--- app/config/config.example.js
+++ app/config/config.example.js
@@ -1,4 +1,4 @@
 module.exports = {
-  url: "mongodb://localhost:27017/cherami",
+  url: "mongodb://localhost:27017/yourdatabase",
   accessTokenSecret: "donotsharethiswithanyone" // do not share this with anyone
 };
