1ffdfc1ae75dd9a8c105ed117aef9d841205317b
Yuvraj Jhala <png625@gmail.com>
2021-04-16 13:03:34 +0530

    Ignored files:

--- app/config/config.example.js
+++ app/config/config.example.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: "mongodb://localhost:27017/yourdatabase",
+  url: "mongodb://localhost:27017/cherami",
+  accessTokenSecret: "donotsharethiswithanyone" // do not share this with anyone
 };
